379b13868b8e31124a9cfe68f584296a53968458
Jiri Bruchanov <jbruchanov@gmail.com>
2023-12-26 16:44:50 +0000

    Update defaults

--- app/src/main/java/com/jibru/obdserver/Defaults.kt
+++ app/src/main/java/com/jibru/obdserver/Defaults.kt
@@ -1,30 +1,31 @@
 package com.jibru.obdserver
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 
 object Defaults {
     val defaults = mutableStateMapOf(
         //@formatter:off
         //\r added in rawData
-        "ATZ"   to mutableListOf("ELM327 v1.5\r\r>"),
-        "ATE0"  to mutableListOf("OK\r\r>"),
-        "ATL0"  to mutableListOf("OK\r\r>"),
-        "ATS0"  to mutableListOf("OK\r\r>"),
-        "ATH0"  to mutableListOf("OK\r\r>"),
-        "ATSP0" to mutableListOf("OK\r\r>"),
-        "ATST62" to mutableListOf("OK\r\r>"),
+        "ATZ"   to mutableStateListOf("ELM327 v1.5\r\r>"),
+        "ATE0"  to mutableStateListOf("OK\r\r>"),
+        "ATL0"  to mutableStateListOf("OK\r\r>"),
+        "ATS0"  to mutableStateListOf("OK\r\r>"),
+        "ATH0"  to mutableStateListOf("OK\r\r>"),
+        "ATSP0" to mutableStateListOf("OK\r\r>"),
+        "ATST62" to mutableStateListOf("OK\r\r>"),
 
-        "010C" to mutableListOf("410C0000\r\r>"),
-        "0146" to mutableListOf("414633\r\r>"),
-        "0146" to mutableListOf("414633\r\r>"),
-        "0901" to mutableListOf("NO DATA\r\r>"),
-        "0902" to mutableListOf("014\r0: 490201574444\r", "1: 32303733303132\r2:", " 46333532373230\r\r>"),
+        "0105" to mutableStateListOf("410580\r\r>"),
+        "010C" to mutableStateListOf("410C0000\r\r>"),
+        "010D" to mutableStateListOf("410D20\r\r>"),
+        "0146" to mutableStateListOf("414633\r\r>"),
+        "0110" to mutableStateListOf("411000FF\r\r>"),
+        "01A6" to mutableStateListOf("41A600000000\r\r>"),
+        "015C" to mutableStateListOf("015C80\r\r>"),
+        "0901" to mutableStateListOf("NO DATA\r\r>"),
+        "0902" to mutableStateListOf("014\r0: 490201574444\r", "1: 32303733303132\r2:", " 46333532373230\r\r>"),
         //@formatter:on
     )
-
-    val rawData = defaults.map {
-        (it.key + "\r") to it.value.map { it.encodeToByteArray() }
-    }.toMap()
 
     val counters = mutableMapOf<String, Int>()
 
@@ -48,11 +49,11 @@ object Defaults {
         }
         val counter = counters.getOrPut(requestStr) { 0 } + 1
         counters[requestStr] = counter
-        val bytes = rawData[requestStr]?.mapIndexed { index, bytes -> bytes.updateBasedOnFlags(index, requestStr) }
-        return bytes ?: listOf("RE:${request.decodeToString()}".encodeToByteArray())
+        val bytes = defaults[requestStr.trim('\r')]?.mapIndexed { index, bytes -> bytes.encodeToByteArray().updateBasedOnFlags(index, requestStr) }
+        return bytes ?: listOf("RE:${request.decodeToString()}\r\r>".encodeToByteArray())
     }
 
-    private fun ByteArray.updateBasedOnFlags(index:Int, requestStr: String): ByteArray {
+    private fun ByteArray.updateBasedOnFlags(index: Int, requestStr: String): ByteArray {
         var msg = decodeToString()
         if (echoOn) {
             msg = requestStr + msg
